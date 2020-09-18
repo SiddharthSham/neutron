b3e31ca83062a4b1f18fd6324e28bfb0febf11dc
siddharthsham <siddhsham@gmail.com>
2020-09-18 15:56:52 +0530

    add network cache

---
+++ js/store.js
@@ -0,0 +1,10 @@
+// Use this module to cache network requests
+
+export const store = {
+  // init to null
+  dashboard: null,
+  blog: {
+    // after data is fetched, persist it here
+    read: null,
+  },
+};
